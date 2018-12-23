8ede62ff7e41544a787bc03a257801f27814d39c
Michał Dziedziński <michal.marcin.dziedzinski@gmail.com>
2018-12-23 09:35:42 +0100

    Added new test

--- minesweeper/src/solution.js
+++ minesweeper/src/solution.js
@@ -1,37 +1,32 @@
+const sapper = function (arr) {
 
+  isParamOk(arr);
 
-const saper = function (arr) {
+  const bombArray = findBomb(arr);
 
-  isParamOk(arr) //dwu wymiarowa tablica
-    // return replaceNumber(arr);
+  bombArray.forEach(el => {
+    arr = calculate(el[0], el[1], arr);
+  });
 
-    const bombArray = findBomb(arr);
-    
-    bombArray.forEach(el => {
-      arr = calculate(el[0], el[1], arr);
-    });
-
-    return arr;
-
-  
-  // else{ //jednowymiarowa tablica 
-  //   throw new Error('it is not 2d array');
-    
-  // }
-
+  return arr;
 };
 
-const isParamOk = (arr) =>{
+const isParamOk = (arr) => {
 
-  if (!Array.isArray(arr)){
-    throwMyError('It is not an array')
+  if (!Array.isArray(arr)) {
+    throwMyError('It is not an array');
   } else if (!arr.length) {
     throw new Error('it is not 2d array');
-  }
-  else{
+  } else {
     arr.forEach(el => {
-      if(!Array.isArray(el)){
-        throwMyError('Inside array there are not an array')
+      if (!Array.isArray(el)) {
+        throwMyError('Inside array there are not only arrays');
+      } else {
+        el.forEach((element, index) => {
+          if(element !== 0 && element !== 'x'){
+            throwMyError(`At least one of incoming values in array is not correct`)
+          }
+        })
       }
     })
   }
@@ -41,14 +36,13 @@ const throwMyError = (err) => {
   throw new Error(err);
 };
 
-const calculate = (x, y, arr)=> {
-  for(let i = x-1; i < x+2; i++){
-    for(let j = y-1; j < y+2; j++){
-      if ((i>=0 && j>=0) && (i < arr.length && j < arr[i].length)){
-        if (isNaN(arr[i][j])){
+const calculate = (x, y, arr) => {
+  for (let i = x - 1; i < x + 2; i++) {
+    for (let j = y - 1; j < y + 2; j++) {
+      if ((i >= 0 && j >= 0) && (i < arr.length && j < arr[i].length)) {
+        if (isNaN(arr[i][j])) {
           continue;
-        }
-        else if(x !== i || y !== j){
+        } else if (x !== i || y !== j) {
           arr[i][j]++;
         }
       }
@@ -57,19 +51,19 @@ const calculate = (x, y, arr)=> {
   return arr;
 }
 
-const findBomb = (arr) =>{
-    let returnArr= [];
-    for(let i=0; i<arr.length; i++){
-      for(let j=0; j<arr[i].length; j++){
-        if (isNaN(arr[i][j])){
-          returnArr.push([i,j]);
-        }
-      } 
+
+const findBomb = (arr) => {
+  let returnArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (isNaN(arr[i][j])) {
+        returnArr.push([i, j]);
+      }
     }
+  }
   return returnArr;
 }
 
-
 exports.calculate = calculate;
 exports.findBomb = findBomb;
-exports.saper = saper;
+exports.sapper = sapper;
--- minesweeper/src/test.js
+++ minesweeper/src/test.js
@@ -1,79 +1,52 @@
-const { saper, calculate, findBomb } = require('./solution');
+const { sapper, calculate, findBomb } = require('./solution');
 
 it('empty array', () => {
-  expect(() => saper([])).toThrow('it is not 2d array');
+  expect(() => sapper([])).toThrow('it is not 2d array');
 });
 
-it('not Array', () => {
-  expect(() => saper('')).toThrow('It is not an array');
+it('Not Array', () => {
+  expect(() => sapper('')).toThrow('It is not an array');
 });
 
-it('Secend element not array', () => {
-  expect(() => saper([[1,1,'x'], ''])).toThrow('Inside array there are not an array');
+it('Are only arrays inside', () => {
+  expect(() => sapper([[0, 0, 'x'], ''])).toThrow('Inside array there are not only arrays');
 });
 
 it('One line vertical, one bomb', () => {
-  expect(saper([['x'], [0]])).toEqual([['x'], [1]]);
+  expect(sapper([['x'], [0]])).toEqual([['x'], [1]]);
 });
 
 it('One bomb 2x2', () => {
-  expect(saper([['x', null], [null, null]])).toEqual([['x', 1], [1, 1]]);
+  expect(sapper([['x', 0], [0, 0]])).toEqual([['x', 1], [1, 1]]);
 });
-
 
 it('one bomb 3x3', () => {
-  expect(calculate(1,1, [[0,0, 0], [0,'x', 0], [0, 0, 0]])).toEqual([[1,1,1],[1,'x', 1], [1, 1, 1]]);
-});
-it('one bomb 3x5', () => {
-  expect(calculate(1,2, [[0,0, 0, 0, 0], [0, 0,'x', 0, 0], [0, 0, 0, 0, 0]])).toEqual([[0,1,1,1,0],[0,1,'x', 1,0], [0,1, 1, 1,0]]);
+  expect(calculate(1, 1, [[0, 0, 0], [0,'x', 0], [0, 0, 0]])).toEqual([[1, 1, 1], [1, 'x', 1], [1, 1, 1]]);
 });
 
-it('one bomb in first corner', () => {
-  expect(calculate(0,0, [['x',0, 0, 0, 0], [0, 0,0, 0, 0], [0, 0, 0, 0, 0]])).toEqual([['x',1,0,0,0],[1,1,0, 0,0], [0,0, 0, 0,0]]);
+it('one bomb 5x5', () => {
+  expect(calculate(1, 2, [[0, 0, 0, 0, 0], [0, 0, 'x', 0, 0], [0, 0, 0, 0, 0]])).toEqual([[0, 1, 1, 1, 0], [0, 1, 'x', 1, 0], [0, 1, 1, 1, 0]]);
 });
 
-
-it('one bomb in last corner ', () => {
-  expect(calculate(2,4, [ [0, 0, 0, 0, 0],
-                          [0, 0, 0 , 0, 0],
-                          [0, 0, 0, 0, 'X']]))
-                          .toEqual([ 
-                              [0, 0, 0, 0, 0],
-                              [0, 0, 0, 1, 1],
-                              [0, 0, 0, 1, 'X']]);
+it('one bomb in top left corner', () => {
+  expect(calculate(0, 0, [['x', 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])).toEqual([['x', 1, 0, 0, 0], [1, 1, 0, 0, 0], [0, 0, 0, 0, 0]]);
 });
+
+it('one bomb in bottom right ', () => {
+  expect(calculate(2, 4, [[0, 0, 0, 0, 0], [0, 0, 0 , 0, 0], [0, 0, 0, 0, 'x']])).toEqual([[0, 0, 0, 0, 0], [0, 0, 0, 1, 1], [0, 0, 0, 1, 'x']]);
+});
+
 it('Find bomb in array', () => {
-  expect(findBomb([ ['x', 0, 0, 0, 0],
-                [0, 'x', 0 , 0, 0],
-                ['x', 0, 0, 0, 0],
-                [0, 0, 0, 0, 0]
-              ]))
-                .toEqual(
-                   [[0,0], [1,1], [2,0]]
-                  );
+  expect(findBomb([['x', 0, 0, 0, 0], [0, 'x', 0 , 0, 0], ['x', 0, 0, 0, 0], [0, 0, 0, 0, 0]])).toEqual([[0, 0], [1, 1], [2, 0]]);
 });
 
-
-it('Final test', () => {
-  expect(saper([ ['x', 0, 0, 0],
-                    [0, 'x' , 0, 0],
-                    ['x', 0, 0, 0],
-                    [0, 0, 0, 0]
-              ]))
-                .toEqual(
-                  [ ['x', 2, 1, 0],
-                    [3, 'x', 1 , 0],
-                    ['x', 2, 1, 0],
-                    [1, 1, 0, 0]]
-                  );
+it('Multiple bombs in array', () => {
+  expect(sapper([['x', 0, 0, 0], [0, 'x', 0, 0], ['x', 0, 0, 0], [0, 0, 0, 0]])).toEqual([['x', 2, 1, 0], [3, 'x', 1 , 0], ['x', 2, 1, 0], [1, 1, 0, 0]]);
 });
 
-
-
-
-
-
-
+it('Are incoming values 0 (not a Bomb) or x (bomb)', () => {
+  expect(() => sapper([['x', 1, 0, 0], [0, 'x', 0, 0], ['x', 0, 0, 0], [0, 0, 0, 0]])).toThrow('At least one of incoming values in array is not correct');
+});
 
 // check ===
 // expect(1).toBe(1)
